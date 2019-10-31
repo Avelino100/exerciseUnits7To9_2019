085d1ba36ef2a888b9603465613bc4a8aafdc8a1
Avelino <avelino03690@gmail.com>
2019-10-31 18:19:15 +0100

    Bad comment file Cpp

---
+++ Exercise001.cpp
@@ -0,0 +1,33 @@
+//Avelino Martinez Rodriguez
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num[10],increments=0,decrements=0,equal=0;
+    
+    cout<<"Introduzca 10 numeros: "<<endl;
+    
+    for(int i=0;i<=9;i++)
+    {
+        cin>>num[i];
+        if(num[i]<0)
+        {
+            cout<<"Reinserte el dato"<<endl;
+            i--;
+        }
+    }
+    for(int i=0;i<9;i++)
+    {
+        if(num[i]>num[i+1])
+            decrements++;
+        else if((num[i]<num[i+1]))
+            increments++;
+        else
+            equal++;
+    }
+    cout<<
+    "Increment: "<<increments<<" Decrement: "<<decrements<<" Equal: "<<equal;
+}
